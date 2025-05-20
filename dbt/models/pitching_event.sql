1d4f9349bd3218ea64dedfd68332a7dc62bb1105
to-ikoma <ikoma.toshithio@gmail.com>
2025-05-20 10:38:10 +0900

    chore(dbt): fix charactor

--- dbt/models/pitching_event.sql
+++ dbt/models/pitching_event.sql
@@ -1,24 +1,28 @@
 SELECT 
   games.owner AS owner,
   situations.pitcher AS pitcher,
+  situations.throw AS throw,
+  games.firstAttackTeam AS first_attack_team,
+  games.secondAttackTeam AS second_attack_team,
   games.date AS date,
-  sequences.breakingBall AS breakingBall,
+  sequences.breakingBall AS breaking_ball,
   situations.title AS title,
   situations.inning AS inning,
-  situations.topOrBottom AS topOrBottom,
+  situations.topOrBottom AS top_or_bottom,
   sequences.strikes AS strikes,
   sequences.balls AS balls,
-  situations.outCounts AS outCounts,
+  situations.outCounts AS out_counts,
   situations.runner AS runner,
-  situations.firstAttackTeamScore AS firstAttackTeamScore,
-  situations.secondAttackTeamScore AS secondAttackTeamScore,
-  sequences.pitchResult AS pitchResult,
+  situations.firstAttackTeamScore AS first_attack_team_score,
+  situations.secondAttackTeamScore AS second_attack_team_score,
+  sequences.pitchResult AS pitch_result,
   sequences.sequence AS sequence,
   situations.batter AS batter,
   sequences.speed AS speed,
-  sequences.pitchXPosition AS pitchXPosition,
-  sequences.pitchYPosition AS pitchYPosition,
-  sequences.id AS id
+  sequences.pitchXPosition AS pitch_x_position,
+  sequences.pitchYPosition AS pitch_y_position,
+  sequences.id AS id,
+  sequences.updatedAt AS updated_at
  FROM default.raw_privategameforgameformat_jqdkca4vcbf6nh7ojsquhjo4ai_staging AS games
  JOIN default.raw_privatesituationforgameformat_jqdkca4vcbf6nh7ojsquhjo4ai_staging AS situations
  ON games.id = situations.gameId
