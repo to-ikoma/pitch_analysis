f7a59119e00e11101edf55c2bc24ef0bd89838dd
to-ikoma <ikoma.toshithio@gmail.com>
2025-05-20 10:53:13 +0900

    feat(dbt/models): add dimention pitcher.

---
+++ dbt/models/dim_pitcher.sql
@@ -0,0 +1,44 @@
+{{
+    config(
+        materialized='incremental'
+    )
+}}
+
+WITH base AS (
+  SELECT
+    owner,
+    pitcher,
+    throw,
+    top_or_bottom,
+    second_attack_team,
+    first_attack_team,
+    CASE
+      WHEN top_or_bottom = 'top' THEN second_attack_team
+      WHEN top_or_bottom = 'bottom' THEN first_attack_team
+      ELSE ''
+    END AS team_for_key
+  FROM {{ ref('pitching_event') }}
+)
+
+SELECT
+  -- サロゲートキー
+  md5(concat_ws('||', owner, pitcher, throw, team_for_key)) AS pitcher_key,
+
+  owner,
+  pitcher AS pitcher_name,
+  throw,
+  team_for_key AS team,
+  'NO' AS is_missing,
+  current_timestamp() AS created_at,
+  'dbt/snapshot' AS created_by,
+  current_timestamp() AS updated_at,
+  'dbt/snapshot' AS updated_by
+
+FROM base
+GROUP BY owner, pitcher, throw, team_for_key
+
+{% if is_incremental() %}
+
+	where "pitcher_key" not in (select "pitcher_key" from {{ this }})
+
+{% endif %}
